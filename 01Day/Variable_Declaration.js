4bef29b6ccb205c4dcfacc5318ad223d3182c66e
Rishav Raj <rishavraj0820@gmail.com>
2024-07-16 14:38:14 +0530

    some changes

--- 01Day/Variable_Declaration.js
+++ 01Day/Variable_Declaration.js
@@ -1,11 +1,11 @@
 var rollNo = 18;
-// console.log(rollNo);
+console.log(rollNo);
 
 let name = "Rishav Raj";
-// console.log(name);
+console.log(name);
 
 const isLoggedIn = true;
-// console.log(isLoggedIn);
+console.log(isLoggedIn);
 
 const userName = "Rishav";
 const userId = 30;
@@ -23,6 +23,13 @@ console.log(typeof(loggedIn));
 console.log(typeof(workingLanguage));
 console.log(typeof(anotherUser));
 
+let color = "red";
+console.log(color);
+
+ color = "orange"
+ console.log(color);
+
 const fruit = "apple";
 console.log(fruit);
-// const fruit = "banana"  // here an error will occur, because i already assign value to the variable fruit, since const is a constant which means its value can't be change.
+ fruit = "banana"  // here an error will occur, because i already assign value to the variable fruit, since const is a constant which means its value can't be change.
+ console.log(fruit);
