3fb9b83a94c4a54358e53b2c6576f7edb5c04b56
Rishav Raj <rishavraj0820@gmail.com>
2024-07-16 13:18:59 +0530

    Understanding Variables and Datatypes.

---
+++ 01Day/Variable_Declaration.js
@@ -0,0 +1,28 @@
+var rollNo = 18;
+// console.log(rollNo);
+
+let name = "Rishav Raj";
+// console.log(name);
+
+const isLoggedIn = true;
+// console.log(isLoggedIn);
+
+const userName = "Rishav";
+const userId = 30;
+const loggedIn = true;
+const workingLanguage = ["Javascript", "Python", "Java"];
+const anotherUser = {
+    userName : "Raj",
+    userId : 32,
+    loggedIn : false
+}
+
+console.log(typeof(userName));
+console.log(typeof(userId));
+console.log(typeof(loggedIn));
+console.log(typeof(workingLanguage));
+console.log(typeof(anotherUser));
+
+const fruit = "apple";
+console.log(fruit);
+// const fruit = "banana"  // here an error will occur, because i already assign value to the variable fruit, since const is a constant which means its value can't be change.
